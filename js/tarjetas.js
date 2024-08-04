9b9fc976e3a427a463b8dd753462136366f6adb7
Federico Ratti <167378653+Federicoratti11@users.noreply.github.com>
2024-08-03 21:02:12 -0300

    Add files via upload

---
+++ js/tarjetas.js
@@ -0,0 +1,31 @@
+
+let productoscontainer = document.querySelector(`#container`)
+function redirectToPage() {
+  window.location.href = 'cart.html'; // Reemplaza 'otra_pagina.html' con la ruta a la página a la que deseas redirigir
+}
+
+Productos.forEach((producto)=>{
+    let copia = document.querySelector(`template`).content.cloneNode(true)
+    copia.querySelector('img').src = `../img/productos/` +producto.img;
+    copia.querySelector(`h3`).textContent = producto.nombre
+    copia.querySelector(`h4`).textContent = producto.marca
+    copia.querySelector(`p`).textContent += producto.precio
+
+    copia.querySelector(`button`).addEventListener("click",() => {
+        agregarAlCarrito(producto);
+
+        Toastify({
+            text: "Producto agregado",
+            duration: 500,
+            className: "info",
+            style: {
+              background: "linear-gradient(to right, rgb(150, 244, 186),rgb(255, 200, 255) )",
+            },
+          }).showToast();
+    })
+
+    productoscontainer.append(copia)
+})
+
+
+let carrito = new carrito
